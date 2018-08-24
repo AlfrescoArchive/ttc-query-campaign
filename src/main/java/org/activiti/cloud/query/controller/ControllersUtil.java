0775d94b163b360e7001aa8ce46505d7d2e73a55
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2018-08-24 13:18:12 +0100

    2nd round of fixing casting issue

--- src/main/java/org/activiti/cloud/query/controller/ControllersUtil.java
+++ src/main/java/org/activiti/cloud/query/controller/ControllersUtil.java
@@ -42,12 +42,18 @@ public class ControllersUtil {
                                                "timestamp");
         VariableEntity attitude = getVariableByName(matchedPI,
                                               "attitude");
+
+        System.out.println("Hello from extractVariables: value:" + timestamp.getValue().toString() +
+                                                        "class:" + timestamp.getValue().getClass() +
+                                                        "name: " + timestamp.getName());
+
+
         if (text != null && author != null) {
             tweets.add(new Tweet(text.getValue(),
                                  author.getValue(),
                                  (lang != null) ? lang.getValue() : "",
                                  (attitude != null) ? attitude.getValue() : "",
-                                 (timestamp != null) ? Long.valueOf(timestamp.getValue()) : new Long(0)));
+                                 (timestamp != null) ? Long.valueOf(timestamp.getValue().toString()) : new Long(0)));
         }
     }
 
