1641d9eb6193bc7f856ac9cabc501c2050fac66d
ryandawsonuk <ryandawson@cantab.net>
2018-12-10 14:08:24 +0000

    Variable is now ProcessVariable

--- src/main/java/org/activiti/cloud/query/repository/ExtendedVariableRepository.java
+++ src/main/java/org/activiti/cloud/query/repository/ExtendedVariableRepository.java
@@ -10,13 +10,13 @@ import org.springframework.data.repository.query.Param;
 
 public interface ExtendedVariableRepository extends VariableRepository {
 
-    @Query("select v from Variable v where v.name= 'matched' " +
+    @Query("select v from ProcessVariable v where v.name= 'matched' " +
                      "and v.processInstance.status='COMPLETED' "+
                      "and v.processInstance.businessKey= :campaign " +
                      "order by v.processInstance.lastModified desc")
     List<ProcessVariableEntity> findAllCompletedAndMatched(@Param("campaign") String campaign);
 
-    @Query("select v from Variable v where v.name= 'matched' " +
+    @Query("select v from ProcessVariable v where v.name= 'matched' " +
             "and v.processInstance.status='COMPLETED' "+
             "and v.processInstance.businessKey= :campaign and v.processInstance.lastModified > :since order by v.processInstance.lastModified desc")
     List<ProcessVariableEntity> findAllCompletedAndMatchedSince(@Param("campaign") String campaign, @Param("since") Date since);
